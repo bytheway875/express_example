62287cc8ad392deadf8e169aa5df06375d0f6311
Shannon Byrne <s.byrne2254@gmail.com>
2015-01-11 17:52:51 -0600

    Create router file for blocks routes

--- routes/blocks.js
+++ routes/blocks.js
@@ -10,5 +10,63 @@ var blocks = {
     'Rotating': 'Moving in a circle around its center'
 };
 
+router.route('/')
+    .get(function(request, response) {
+        var blockNames = Object.keys(blocks);
+        if (request.query.limit >= 0) {
+            response.json(blockNames.slice(0, request.query.limit));
+        } else if(request.query.search) {
+            response.send(blockSearch(request.query.search));
+        }
+        else {
+            blockNames = Object.keys(blocks);
+            response.json(blockNames);
+        }
+    })
+    .post(parseUrlencoded, function(request, response){
+        var newBlock = request.body;
+        blocks[newBlock.name] = newBlock.description;
+
+        response.status(201).json(newBlock.name);
+
+    });
+
+router.route('/:name')
+    .all(function(request,response, next) {
+        var name = request.params.name;
+        var titleizedName = name[0].toUpperCase() + name.slice(1).toLowerCase();
+        request.titleizedName = titleizedName;
+
+        next();
+    })
+    .get(function(request, response) {
+        var description = blocks[request.titleizedName];
+        if(description){
+            response.json(description);
+        }else{
+            response.status(404).json("Uh oh. Can't find anything here for " + request.params.name);
+        }
+    })
+    .delete(function(request, response){
+        if(blocks[request.titleizedName]) {
+            var deleted = delete blocks[request.titleizedName];
+            if (deleted === true) {
+                response.sendStatus(200);
+            } else {
+                response.sendStatus(500);
+            }
+        }else{
+            response.sendStatus(404);
+        }
+    });
+
+function blockSearch (keyword) {
+    var regexp = RegExp(keyword, 'i');
+    var result = blockNames.filter(function (block) {
+        return block.match(regexp);
+    });
+
+    return result;
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,10 +2,11 @@ var express = require('express');
 var app = express();
 
 var logger = require('./logger');
-app.use(logger);
+var blocks = require('./routes/blocks');
 
-var bodyParser = require('body-parser');
-var parseUrlencoded = bodyParser.urlencoded({ extended: false});
+app.use(express.static('public'));
+app.use(logger);
+app.use('/blocks', blocks);
 
 // Point route directly to specific file
 
@@ -13,76 +14,9 @@ var parseUrlencoded = bodyParser.urlencoded({ extended: false});
 //    res.sendFile(__dirname + '/public/index.html')
 //});
 
-var blocks = {
-    'Fixed' : 'Fastened securely in position',
-    'Movable' : 'Capable of being moved',
-    'Rotating' : 'Moving in a circle around its center'
-};
-
-app.use(express.static('public'));
-
-
-app.param('name', function(request, response, next){
-    var name = request.params.name
-    var titleizedName = name[0].toUpperCase() + name.slice(1).toLowerCase();
-    request.titleizedName = titleizedName;
-
-    next();
-});
-
-app.route('/blocks')
-    .get(function(request, response) {
-        var blockNames = Object.keys(blocks);
-        if (request.query.limit >= 0) {
-            response.json(blockNames.slice(0, request.query.limit));
-        } else if(request.query.search) {
-            response.send(blockSearch(request.query.search));
-        }
-        else {
-            blockNames = Object.keys(blocks);
-            response.json(blockNames);
-        }
-    })
-    .post(parseUrlencoded, function(request, response){
-        var newBlock = request.body;
-        blocks[newBlock.name] = newBlock.description;
-
-        response.status(201).json(newBlock.name);
-
-    });
-
-app.route('/blocks/:name')
-    .get(function(request, response) {
-        var description = blocks[request.titleizedName];
-        if(description){
-            response.json(description);
-        }else{
-            response.status(404).json("Uh oh. Can't find anything here for " + request.params.name);
-        }
-    })
-    .delete(function(request, response){
-        if(blocks[request.blockName]) {
-            var deleted = delete blocks[request.blockName];
-            if (deleted === true) {
-                response.sendStatus(200);
-            } else {
-                response.sendStatus(500);
-            }
-        }else{
-            response.sendStatus(404);
-        }
-    });
 
 
 
-function blockSearch (keyword) {
-    var regexp = RegExp(keyword, 'i');
-    var result = blockNames.filter(function (block) {
-        return block.match(regexp);
-    });
-
-    return result;
-}
 
 //app.get('/array', function(request, response) {
 //    var blocks = ['Fixed', 'Movable', 'Rotating'];
